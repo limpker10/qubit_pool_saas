7f4defdf2ef87ed1e8c9046f04ba1985d5952dd3
JOSE MIGUEL CANO VILCAPAZA <jcanov@unsa.edu.pe>
2025-09-02 09:15:03 -0500

    agrego nuevo archivo en el gitignore

--- config/tenancy.php
+++ config/tenancy.php
@@ -34,6 +34,7 @@ return [
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\QueueTenancyBootstrapper::class,
         // Stancl\Tenancy\Bootstrappers\RedisTenancyBootstrapper::class, // Note: phpredis is needed
+
     ],
 
     /**
@@ -136,7 +137,7 @@ return [
          * disable asset() helper tenancy and explicitly use tenant_asset() calls in places
          * where you want to use tenant-specific assets (product images, avatars, etc).
          */
-        'asset_helper_tenancy' => true,
+        'asset_helper_tenancy' => false,
     ],
 
     /**
